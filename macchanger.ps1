b781476d5ca47a5fcc63c8f5a39e607467c5dbc0
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2022-09-09 02:52:21 +0900

    Add files via upload

---
+++ macchanger.ps1
@@ -0,0 +1,37 @@
+﻿Write-Host -BackgroundColor black -ForegroundColor green 'Showing Net Adapters, please pick one'
+sleep -Seconds 2
+
+Get-NetAdapter
+
+sleep -Seconds 2 
+$mac = Read-Host -Prompt 'Adapter to change?'
+
+$confirmation = Read-Host "Do you want a random MAC or do you have one? (Random/MyOwn):"
+        if ($confirmation -eq 'Random')
+	{
+		$change = for($i=1; $i -le 1; $i++){([char[]]([char]'A'..[char]'F') + 0..9 | sort {Get-Random})[0..11] -join ''}
+
+		Set-NetAdapter -Name $mac -MacAddress $change -Confirm:$false
+
+		Write-Host -BackgroundColor black -ForegroundColor green 'Waiting 5 seconds to restart network adapter'
+		sleep -Seconds 5
+
+		Write-Host -BackgroundColor black -ForegroundColor green 'Showing Net Adapter'$mac
+		Get-NetAdapter -name $mac
+	}
+
+	elseif ($confirmation -eq 'MyOwn')
+	{
+		$mac_address = Read-Host -Prompt 'Mac Address'
+		Set-NetAdapter -Name $mac -MacAddress $mac_address -Confirm:$false
+
+		Write-Host -BackgroundColor black -ForegroundColor green 'Waiting 5 seconds to restart network adapter'
+		sleep -Seconds 5
+
+		Write-Host -BackgroundColor black -ForegroundColor green 'Showing Net Adapter'$mac
+		Get-NetAdapter -name $mac
+	}
+
+	else {}
+
+		
